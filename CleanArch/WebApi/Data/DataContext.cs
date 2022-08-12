75400c9d4f6d0e64516e6a9d0f131c2e9c33b865
rozdemir23 <80854878+rozdemir23@users.noreply.github.com>
2022-08-12 21:06:45 +0300

    Add files via upload

---
+++ CleanArch/WebApi/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApi.Models.Urun> Urun { get; set; }
+    }
+}
